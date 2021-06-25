e3f64952702bdd6e723491f666cf31cef7629915
Suyog-Dhakal <075bct092.suyog@pcampus.edu.np>
2021-06-25 16:40:01 +0545

    updated

--- lab1/fourierseries_b.m
+++ lab1/fourierseries_b.m
@@ -1,3 +1,28 @@
-t = [-5:0.01:5]
-g= -t^4+17*t^3-t^2-47;
-plot(t,g);
+L = 5;
+t = -L : 0.01 : L;
+g = @ (t) -t.^4 + 17 * t.^3 - t.^2 - 47;
+g_t = g(t);
+
+subplot(2, 1, 1);
+plot(t, g_t);
+xlabel("t");
+ylabel("g(t)");
+
+m = 20;
+Ao = 1 / L * trapz(t, g_t);
+A  = zeros(1, m);
+B  = zeros(1, m);
+S  = Ao / 2;
+
+for i = 1 : m
+    A(i) = 1 / L * trapz(t, g_t .* cos( (i * pi / L) .* t));
+    B(i) = 1 / L * trapz(t, g_t .* sin( (i * pi / L) .* t));
+    S   += (A(i) .* cos( (i * pi / L) .* t) +
+            B(i) .* sin( (i * pi / L) .* t));
+endfor
+
+subplot(2, 1, 2);
+plot(t, S);
+title("Using Fourier Series");
+xlabel("t");
+ylabel("g(t)");
